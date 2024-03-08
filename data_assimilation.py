d1b45eba270491eb488e9f4f345bd902c983be07
Paulo Sergio <paulosergio0414918@gmail.com>
2024-03-07 22:51:31 -0400

    INSERINDO OS MÉTODOS NUMERICOS neste commit foram inserios os métodos numéricos para resolução da equação da advecção para obtenção dos dados a serem utilizados na assimilação de dados.

---
+++ data_assimilation.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# things to do
+# numerical resolution to advection equation
+# study data assimilaton metods
+
+# advection equation
+
+
+
+#Lax-Friedrishs Method
+
+
+#leap-frog method
+
+
+#Graphic representation of solution
+
+
+#Data Assimilation for advection equation
